d66a1473e5513a6d67e73ba27c6f724b7dd6f90a
gblair.onsite <gblair.onsite@gmail.com>
2026-05-26 15:51:50 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,7 @@
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import Services from '../components/Services';
-import Portfolio from '../components/Portfolio';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-
 export default function Home() {
   return (
-    <div className="min-h-screen font-body">
-      <Navbar />
-      <Hero />
-      <Services />
-      <Portfolio />
-      <Contact />
-      <Footer />
+    <div className="min-h-screen bg-background">
+      {/* Blank page */}
     </div>
   );
 }
