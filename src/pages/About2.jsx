013c995b68f58da703eb7389ecee19fd2411be67
OnSiteComputers <161184349+OnSiteComputers@users.noreply.github.com>
2026-06-14 22:38:57 -0400

    Delete src directory

--- src/pages/About2.jsx
+++
@@ -1,2 +0,0 @@
-import About from './About';
-export default function About2() { return <About />; }
--- src/pages/Contact2.jsx
+++
@@ -1,2 +0,0 @@
-import Contact from './Contact';
-export default function Contact2() { return <Contact />; }
